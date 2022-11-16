70baf7adf6fbdb3d056cddd4647674ba97df2b44
Murillo <murillohsantos@gmail.com>
2022-11-16 17:31:22 -0300

    requisito 2 talvez resolvido

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,6 +2,14 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
-  // seu código aqui
+export const fetchProductsList = (product) => {
+  if (!product) throw new Error('Termo de busca não informado');
+
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${product}`;
+
+  return fetch(url)
+    .then((response) => response.json())
+    .then(console.log);
 };
+
+console.log(fetchProductsList('computador'));
