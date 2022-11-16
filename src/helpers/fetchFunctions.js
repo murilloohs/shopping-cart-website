1f5e5e80f811bb72fa66299a704dbf5cca03e5bc
Murillo <murillohsantos@gmail.com>
2022-11-16 18:50:28 -0300

    arrumando alguns erros

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -1,17 +1,15 @@
-import fetch from 'node-fetch';
-
 export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = (product) => {
+export const fetchProductsList = async (product) => {
   if (!product) throw new Error('Termo de busca não informado');
 
   const url = `https://api.mercadolibre.com/sites/MLB/search?q=${product}`;
 
-  return fetch(url)
-    .then((response) => response.json())
-    .then(console.log);
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
 };
 
 console.log(fetchProductsList('computador'));
