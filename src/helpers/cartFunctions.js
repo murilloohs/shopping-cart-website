1c930764f524b45d7e640768a57e25271be22589
Murillo <murillohsantos@gmail.com>
2023-12-06 00:14:50 -0300

    preço somando e alguns bugs concertados

--- src/helpers/cartFunctions.js
+++ src/helpers/cartFunctions.js
@@ -3,18 +3,40 @@ export const getSavedCartIDs = () => {
   return cartProducts ? JSON.parse(cartProducts) : [];
 };
 
-export const saveCartID = (id) => {
+export const getSavedCartPrice = () => {
+  const cartPrice = localStorage.getItem('price');
+  return cartPrice ? JSON.parse(cartPrice) : [];
+};
+
+export const saveCartID = (id, price) => {
   if (!id) throw new Error('Você deve fornecer um ID');
 
   const cartProducts = getSavedCartIDs();
   const newCartProducts = [...cartProducts, id];
   localStorage.setItem('cartProducts', JSON.stringify(newCartProducts));
+
+  const cartPrice = getSavedCartPrice();
+  const newCartPrice = [...cartPrice, price];
+  localStorage.setItem('price', JSON.stringify(newCartPrice));
 };
 
 export const removeCartID = (id) => {
   if (!id) throw new Error('Você deve fornecer um ID');
 
+  function removeFirstOccurrence(arr, reference) {
+    const index = arr.indexOf(reference);
+    const one = -1;
+    if (index !== one) {
+      arr.splice(index, 1);
+    }
+    return arr;
+  }
+
   const cartProducts = getSavedCartIDs();
-  const newCartProducts = cartProducts.filter((product) => product !== id);
+  const newCartProducts = removeFirstOccurrence(cartProducts, id);
   localStorage.setItem('cartProducts', JSON.stringify(newCartProducts));
+
+  // const cartPrice = getSavedCartPrice();
+  // const newCartPrice = removeFirstOccurrence(cartPrice, price);
+  // localStorage.setItem('price', JSON.stringify(newCartPrice));
 };
--- src/main.js
+++ src/main.js
@@ -20,15 +20,48 @@ const removeLoadText = () => {
   productsList.removeChild(loadingText);
 };
 
+const totalElement = document.querySelector('.total-price');
+let prices = [0];
+let indexLi = 0;
+
+const onloadPrices = () => {
+  let storagePrices = JSON.parse(localStorage.getItem('price'));
+  if (storagePrices === null) {
+    storagePrices = [];
+  }
+  prices = storagePrices;
+
+  let price = storagePrices.reduce((total, actual) => total + actual, 0);
+  totalElement.innerText = price;
+
+  const li = document.querySelectorAll('.cart__product');
+
+  li.forEach((element, index) => {
+    element.addEventListener('click', () => {
+      storagePrices = JSON.parse(localStorage.getItem('price'));
+      const priceIten = storagePrices[index + 1];
+
+      prices = storagePrices.filter((number) => number !== priceIten);
+      price = prices.reduce((total, actual) => total + actual, 0);
+
+      totalElement.innerText = price;
+      localStorage.setItem('price', JSON.stringify(prices));
+    });
+  });
+  if (li === 0) {
+    totalElement.innerText = 0;
+  }
+};
+
 try {
   createLoadText();
   const products = await fetchProductsList('computador');
   const addProducts = () => {
     products.forEach((element) => {
       const { id, title, thumbnail, price } = element;
-
       productsList.appendChild(createProductElement({ id, title, thumbnail, price }));
     });
+    onloadPrices();
     removeLoadText();
   };
   addProducts();
@@ -43,22 +76,47 @@ try {
 const addToCartBtn = document.querySelectorAll('.product__add');
 const cartList = document.querySelector('.cart__products');
 
-const addToCart = async (element) => {
-  const { target } = element;
-  const id = target.parentElement.firstChild.innerHTML;
-
-  saveCartID(id);
-
-  const productData = await fetchProduct(id);
-  const cartProduct = createCartProductElement(productData);
-  cartList.appendChild(cartProduct);
-};
-
-addToCartBtn.forEach((element) => element.addEventListener('click', addToCart));
-
 const productInCart = () => getSavedCartIDs().forEach(async (elementId) => {
   const data = await fetchProduct(elementId);
   cartList.appendChild(createCartProductElement(data));
 });
 
 productInCart();
+
+const totalPrice = (p) => {
+  prices.push(p);
+  localStorage.setItem('price', JSON.stringify(prices));
+  const storagePrices = JSON.parse(localStorage.getItem('price'));
+  const price = storagePrices.reduce((total, actual) => total + actual);
+  totalElement.innerText = price.toFixed(2);
+};
+
+const removePrice = (p) => {
+  const li = document.querySelectorAll('.cart__product');
+  li[indexLi].addEventListener('click', () => {
+    prices = prices.filter((number) => number !== p);
+    localStorage.setItem('price', JSON.stringify(prices));
+    const storagePrices = JSON.parse(localStorage.getItem('price'));
+    const price = storagePrices.reduce((total, actual) => total + actual, 0);
+    totalElement.innerText = price.toFixed(2);
+    indexLi -= 1;
+  });
+};
+
+const addToCart = async (element) => {
+  const { target } = element;
+  const id = target.parentElement.firstChild.innerHTML;
+
+  const productData = await fetchProduct(id);
+  const cartProduct = createCartProductElement(productData);
+  cartList.appendChild(cartProduct);
+
+  const p = productData.price;
+
+  saveCartID(id, p);
+  totalPrice(p);
+  removePrice(p);
+  indexLi += 1;
+};
+
+addToCartBtn.forEach((element) => element.addEventListener('click', addToCart));
